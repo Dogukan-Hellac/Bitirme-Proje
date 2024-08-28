8baabdf9e8f03400b50bea314a3075c66da4090d
Dogukan HELLLAC <helac.dogukan@gmail.com>
2024-08-28 14:18:09 +0300

    CREATE Screens

---
+++ screens/LogInScreen/LogInScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './LogInScreen.style'
+
+export default function LogInScreen() {
+  return (
+    <View>
+      <Text>LogInScreen</Text>
+    </View>
+  )
+}
---
+++ screens/SignUpScreen/SignUpScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './SignUpScreen.style'
+
+export default function SignUpScreen() {
+  return (
+    <View>
+      <Text>SignUpScreen</Text>
+    </View>
+  )
+}
