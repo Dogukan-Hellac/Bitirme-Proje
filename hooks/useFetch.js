15341c5731ff94f7cb36336a1f59d09f584ee9d0
Dogukan HELLLAC <helac.dogukan@gmail.com>
2024-08-29 17:45:26 +0300

    CREATE hooks

---
+++ hooks/useFetch.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const fetchData = async (endpoint, params = {}) => {
+  try {
+    const response = await axios.get(endpoint, { params });
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching data from ${endpoint}:`, error);
+    throw error
+  }
+};
+
+export const getCategories = () => {
+  return fetchData('https://store.vrunibex.com/mobile2/mbProduct/CategoryList');
+};
+
+export const getProductsByCategory = (categoryId) => {
+  return fetchData('https://store.vrunibex.com/mobile2/mbProduct/ProductList', { CategoryID: categoryId });
+};
+
+export const getProductDetail = (productId) => {
+  return fetchData('https://store.vrunibex.com/mobile2/mbProduct/ProductDetail', { productId });
+};
--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -1,15 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import styles from './HomeScreen.style'
 
 import CategoryItem from '../../components/CategoryItem'
 
+import { getCategories } from '../../hooks/useFetch'
+
 export default function HomeScreen() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    getCategories()
+      .then(data => setData(data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <CategoryItem text='ELEKTRONİK'/>
-      <CategoryItem text='BEYAZ EŞYA'/>
-      <CategoryItem text='ANKASTRE'/>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => (item.id)}
+        numColumns={3}
+        renderItem={({ item }) => {
+          return (
+            <CategoryItem />
+          )
+        }
+        }
+      />
     </View>
   )
 }
